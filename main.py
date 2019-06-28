3708e8ed27196ce3600a42dcbedd270f1b215709
pclucas14 <lucas.page-caccia@mail.mcgill.ca>
2019-06-28 09:12:51 -0400

    shuffled some utils around

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.distributions as D
 
+from pydoc import locate
 from os.path import join
 from tensorboardX import SummaryWriter
 from torchvision.utils import save_image
-from collections import OrderedDict as OD
 from torchvision import datasets, transforms, utils
 
 from vqvae  import * 
@@ -15,7 +15,7 @@ from utils  import *
 # arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--lr', type=float, default=1e-3)
-parser.add_argument('--n_epochs', type=int, default=1000)
+parser.add_argument('--n_epochs', type=int, default=500)
 parser.add_argument('--batch_size', type=int, default=64)
 parser.add_argument('--device', type=str, choices=['cuda', 'cpu'], default='cuda')
 parser.add_argument('--debug', action='store_true')
@@ -32,20 +32,24 @@ parser.add_argument('--embed_dim', type=int, default=64,        help='size of an
 parser.add_argument('--ema', type=int, default=0)
 parser.add_argument('--gamma', type=float, default=0.99)
 parser.add_argument('--commit_coef', type=float, default=0.5)
+parser.add_argument('--loss_fn', type=str, default='logistic', choices=['logistic', 'gaussian'])
+
 args = parser.parse_args()
 args.input_size = (3, 32, 32)
 args.downsample = args.input_size[-1] // args.hH
 
+# get loss functions
+loss_fn = locate('utils.%s_ll' % args.loss_fn)
+import pdb; pdb.set_trace()
+
 # create model and ship to GPU
+test = load_model_from_file('runs/LR0.001_BS64_D64_K512_hH8_NC1_coef0.5')
 model = VQVAE(args).cuda() 
 print(model)
 print("number of parameters:", sum([np.prod(p.size()) for p in model.parameters()]))
 
 # reproducibility is da best
-np.random.seed(0)
-torch.manual_seed(0)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+set_seed(0)
 
 opt = torch.optim.Adam(model.parameters(), lr=args.lr)
 
@@ -60,22 +64,18 @@ train_loader = torch.utils.data.DataLoader(datasets.CIFAR10('../cl-pytorch/data'
 test_loader  = torch.utils.data.DataLoader(datasets.CIFAR10('../cl-pytorch/data', train=False, 
     download=True, transform=ds_transforms), batch_size=args.batch_size, shuffle=True, **kwargs)
 
-# create logging containers
-def reset_log():
-    logs = OD()
-    for name in ['log p(x|z)', 'commit', 'vq', 'kl', 'bpd', 'elbo']:
-        logs[name] = []
-    return logs
-
 # spawn writer
-model_name = 'LR{}_BS{}_D{}_K{}_hH{}_NC{}_coef{}'.format(args.lr, args.batch_size, args.embed_dim, args.n_embeds,
-                                                         args.hH, args.n_codebooks, args.commit_coef)
+model_name = 'LR{}_BS{}_D{}_K{}_hH{}_NC{}_coef{}_Loss{}'.format(args.lr, args.batch_size, args.embed_dim, args.n_embeds,
+                                                                args.hH, args.n_codebooks, args.commit_coef, args.loss_fn)
 model_name = 'test' if args.debug else model_name
 log_dir    = join('runs', model_name)
 sample_dir = join(log_dir, 'samples')
 writer     = SummaryWriter(log_dir=log_dir)
+
+print_and_save_args(args, log_dir)
 print('logging into %s' % log_dir)
 maybe_create_dir(sample_dir)
+best_test = float('inf')
 
 # useful to track
 KL = args.hH * args.hH * args.n_codebooks * np.log(args.n_embeds)
@@ -88,13 +88,13 @@ for epoch in range(args.n_epochs):
 
     for batch_idx, (input,_) in enumerate(train_loader):
         input = input.cuda()
-        x, loss_quant, loss_commit, _, _ = model(input)
+        x, latent_loss = model(input)
 
-        log_pxz = discretized_logistic(x, model.dec_log_stdv, sample=input).mean()
-        loss = -1 * (log_pxz / N) + args.commit_coef * loss_quant + loss_commit
-       
-        elbo = - (KL - log_pxz) / N
-        bpd  = elbo / np.log(2.)
+        log_pxz = loss_fn(x, model.dec_log_stdv, input).mean()
+        loss = -1 * (log_pxz / N) + args.commit_coef * latent_loss
+
+        elbo = (KL - log_pxz) / N
+        bpd  = (elbo + (np.log(256) if 'gaussian' in args.loss_fn else 0)) / np.log(2.)
      
         opt.zero_grad()
         loss.backward()
@@ -103,7 +103,7 @@ for epoch in range(args.n_epochs):
         train_log['kl']         += [KL]
         train_log['bpd']        += [bpd.item()]
         train_log['elbo']       += [elbo.item()]
-        train_log['commit']     += [loss_quant.item()]
+        train_log['commit']     += [latent_loss.item()]
         train_log['log p(x|z)'] += [log_pxz.item()]
         
 
@@ -117,17 +117,17 @@ for epoch in range(args.n_epochs):
     with torch.no_grad():
         for batch_idx, (input,_) in enumerate(test_loader):
             input = input.cuda()
-            x, loss_quant, loss_commit, _, _ = model(input)
+            x, latent_loss = model(input)
 
-            log_pxz = discretized_logistic(x, model.dec_log_stdv, sample=input).mean()
+            log_pxz = loss_fn(x, model.dec_log_stdv, input).mean()
            
-            elbo = - (KL - log_pxz) / N
-            bpd  = elbo / np.log(2.)
+            elbo = (KL - log_pxz) / N
+            bpd  = (elbo + (np.log(256) if 'gaussian' in args.loss_fn else 0)) / np.log(2.)
             
             test_log['kl']         += [KL]
             test_log['bpd']        += [bpd.item()]
             test_log['elbo']       += [elbo.item()]
-            test_log['commit']     += [loss_quant.item()]
+            test_log['commit']     += [latent_loss.item()]
             test_log['log p(x|z)'] += [log_pxz.item()]
             
         # save reconstructions
@@ -140,3 +140,10 @@ for epoch in range(args.n_epochs):
     for key, value in test_log.items():
         print_and_log_scalar(writer, 'test/%s' % key, value, epoch)
     print()
+
+    current_test = sum(test_log['bpd']) / batch_idx
+    if current_test < best_test:
+        best_test = current_test
+        print('saving best model')
+        torch.save(model.state_dict(), join(log_dir, 'best_model.pth'))
+
--- utils.py
+++ utils.py
@@ -1,15 +1,44 @@
 import os
+import json
 import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.distributions as D
 import torch.nn.utils.weight_norm as wn
+from collections import OrderedDict as OD
 
-def discretized_logistic(mean, logscale, binsize=1 / 256.0, sample=None):
-    scale = torch.exp(logscale)
-    sample = (torch.floor(sample / binsize) * binsize - mean) / scale
-    logp = torch.log(torch.sigmoid(sample + binsize / scale) - torch.sigmoid(sample) + 1e-7)
-    return logp.sum(dim=(1,2,3))
+# good'ol utils
+# ---------------------------------------------------------------------------------
+class dotdict(dict):
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+def print_and_save_args(args, path):
+    print(args)
+    # let's save the args as json to enable easy loading
+    with open(os.path.join(path, 'args.json'), 'w') as f: 
+        json.dump(vars(args), f)
+
+def load_model_from_file(path):
+    with open(os.path.join(path, 'args.json'), 'rb') as f:
+        args = dotdict(json.load(f))
+
+    from vqvae import VQVAE    
+    # create model
+    model = VQVAE(args)
+
+    # load weights
+    model.load_state_dict(torch.load(os.path.join(path, 'best_model.pth')))
+
+    return model
+
+def set_seed(seed):
+    np.random.seed(0)
+    torch.manual_seed(0)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
 
 def print_and_log_scalar(writer, name, value, write_no, end_token=''):
     if isinstance(value, list):
@@ -28,3 +57,31 @@ def print_and_log_scalar(writer, name, value, write_no, end_token=''):
 def maybe_create_dir(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
+
+# create logging containers
+def reset_log():
+    # TODO: make it such that if key is not in dict, it's init with incoming content
+    # this way, we won't have to know in advance what we want to monitor
+    logs = OD()
+    for name in ['log p(x|z)', 'commit', 'vq', 'kl', 'bpd', 'elbo']:
+        logs[name] = []
+    return logs
+
+
+# loss functions 
+# ---------------------------------------------------------------------------------
+def logistic_ll(mean, logscale, sample, binsize=1 / 256.0):
+    # actually discretized logistic, but who cares
+    scale = torch.exp(logscale)
+    sample = (torch.floor(sample / binsize) * binsize - mean) / scale
+    logp = torch.log(torch.sigmoid(sample + binsize / scale) - torch.sigmoid(sample) + 1e-7)
+    return logp.sum(dim=(1,2,3))
+
+def gaussian_ll(mean, logscale, sample):
+    logscale = logscale.expand_as(mean)
+    dist = D.Normal(mean, torch.exp(logscale))
+    logp = dist.log_prob(sample)
+    return logp.sum(dim=(1,2,3))
+    
+
